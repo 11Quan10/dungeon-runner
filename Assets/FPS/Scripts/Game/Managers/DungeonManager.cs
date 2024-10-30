604407523c7c3035c253918f6e48930ecc40bf9f
Quan Pham <quanpham1011@gmail.com>
2024-10-30 00:07:58 -0500

    basic randomized hallway generation

--- Assets/FPS/Scripts/Game/Managers/DungeonManager.cs
+++ Assets/FPS/Scripts/Game/Managers/DungeonManager.cs
@@ -2,21 +2,26 @@ using UnityEngine;
 
 namespace Unity.FPS.Game
 {
+    [System.Serializable]
     public class DungeonManager : MonoBehaviour
     {
+        public GameObject startSectionPrefab;       // Prefab for starting section
         public GameObject straightSectionPrefab;  // Prefab for straight section
-        public GameObject turnSectionPrefab;      // Prefab for corner section
+        public GameObject leftTurnSectionPrefab;        // Prefab for left turn section
+        public GameObject rightTurnSectionPrefab;      // Prefab for right turn section
         public GameObject Player;                  // Player transform to track movement
-        public int sectionLength = 10;            // Length of each section
-        public int maxGeneratedSections = 20;     // Number of sections to generate ahead
+        public int maxGeneratedSections = 1;     // Number of sections to generate ahead
 
+        private int sectionLength = 18;            // Length of each section
         private Vector3 lastPosition;
         private Vector3 currentDirection = Vector3.forward;
-        private int sectionsGenerated = 0;
+        private int sectionsGenerated = 0; 
+        private int currentTurns = 0;               // Current number of turns, used to indicate direction
+        private bool startSection = true;
 
         void Start()
         {
-            lastPosition = Vector3.zero; // Starting point of the dungeon
+            lastPosition = new Vector3(-35, 0, -10); // Starting point of the dungeon
             GenerateSection();
         }
 
@@ -27,10 +32,7 @@ namespace Unity.FPS.Game
             // {
             //     GenerateSection();
             // }
-            for (int i = 0; i < 5; i++) 
-            {
-                GenerateSection();
-            }
+            GenerateSection();
         }
 
         void GenerateSection()
@@ -41,26 +43,73 @@ namespace Unity.FPS.Game
             float randomValue = Random.Range(0f, 1f);
             GameObject newSection;
 
-            if (randomValue < 0.7f) // 70% chance for a straight section
-            {
-                newSection = Instantiate(straightSectionPrefab, lastPosition, Quaternion.LookRotation(currentDirection));
+            if (startSection == true) {
+                Debug.Log("Start Position: " + lastPosition);
+                newSection = Instantiate(startSectionPrefab, lastPosition, Quaternion.LookRotation(currentDirection));
                 lastPosition += currentDirection * sectionLength; // Move to the next section position
-            }
-            else // 30% chance for a 90-degree turn
-            {
-                newSection = Instantiate(turnSectionPrefab, lastPosition, Quaternion.LookRotation(currentDirection));
-                lastPosition += currentDirection * sectionLength;
 
-                // Randomly decide whether to turn left or right
+                startSection = false;
+                sectionsGenerated++;
+                return;
+            }
+            
+            if (randomValue < 0.7f) // 55% chance for a straight section
+            {
+                Debug.Log("Straight Position: " + lastPosition);
+                newSection = Instantiate(straightSectionPrefab, lastPosition, Quaternion.LookRotation(currentDirection));
+            }
+            else                    // 30% chance for a 90-degree turn
+            {
+                // if (Random.value < 0.5f)
+                // {
+                //     Debug.Log("Left Position: " + lastPosition);
+                //     newSection = Instantiate(leftTurnSectionPrefab, lastPosition, Quaternion.LookRotation(currentDirection));
+                //     currentDirection = Quaternion.Euler(0, -90, 0) * currentDirection;  // Left turn
+                // }
+                // else
+                // {
+                //     Debug.Log("Right Position: " + lastPosition);
+                //     newSection = Instantiate(rightTurnSectionPrefab, lastPosition, Quaternion.LookRotation(currentDirection));
+                //     currentDirection = Quaternion.Euler(0, 90, 0) * currentDirection;   // Right turn
+                // }
+
                 if (Random.value < 0.5f)
                 {
-                    currentDirection = Quaternion.Euler(0, -90, 0) * currentDirection; // Left turn
+                    if (currentTurns >= 2) 
+                    {
+                        newSection = Instantiate(rightTurnSectionPrefab, lastPosition, Quaternion.LookRotation(currentDirection));
+                        currentDirection = Quaternion.Euler(0, 90, 0) * currentDirection;   // Right turn
+
+                        currentTurns--;
+                    }
+                    else 
+                    {
+                        newSection = Instantiate(leftTurnSectionPrefab, lastPosition, Quaternion.LookRotation(currentDirection));
+                        currentDirection = Quaternion.Euler(0, -90, 0) * currentDirection;  // Left turn
+
+                        currentTurns++;
+                    }
                 }
                 else
                 {
-                    currentDirection = Quaternion.Euler(0, 90, 0) * currentDirection; // Right turn
+                    if (currentTurns <= -2)
+                    {
+                        newSection = Instantiate(leftTurnSectionPrefab, lastPosition, Quaternion.LookRotation(currentDirection));
+                        currentDirection = Quaternion.Euler(0, -90, 0) * currentDirection;  // Left turn
+
+                        currentTurns++;
+                    }
+                    else
+                    {
+                        newSection = Instantiate(rightTurnSectionPrefab, lastPosition, Quaternion.LookRotation(currentDirection));
+                        currentDirection = Quaternion.Euler(0, 90, 0) * currentDirection;   // Right turn
+
+                        currentTurns--;
+                    }
+                    
                 }
             }
+            lastPosition += currentDirection * sectionLength;
 
             sectionsGenerated++;
         }
